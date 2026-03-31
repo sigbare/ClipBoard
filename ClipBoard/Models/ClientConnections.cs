a75cde82c14e32bdfe7cfcb73db4d87c802d3949
unknown <C:\Users\PK\AppData\Roaming\The Bat!>
2026-03-31 16:35:19 +0300

    _

---
+++ ClipBoard/Models/ClientConnections.cs
@@ -0,0 +1,20 @@
+using System.Net.Sockets;
+
+namespace ClipBoard.Models;
+
+
+public class ClientConnection : IDisposable
+{
+    public string Id { get; set; } = string.Empty;
+    public TcpClient? TcpClient { get; set; }
+    public NetworkStream? Stream { get; set; }
+    public DateTime ConnectedAt { get; set; }
+    public DateTime LastActivity { get; set; }
+    public Dictionary<string, object>? Metadata { get; set; }
+    
+    public void Dispose()
+    {
+        Stream?.Dispose();
+        TcpClient?.Dispose();
+    }
+}
